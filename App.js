43b793b624d03378ded383ce6eb274dd1d1054aa
Min Kim <tmdals0708@gmail.com>
2023-03-17 03:28:13 +0900

    first comit

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function App() {
+  const [permission, setPermission] = useState(false);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState();
+  const [barcodeNum, setBarcodeNum] = useState(null);
+
+  const askForCameraPermission = async () => {
+    const { granted } = await BarCodeScanner.requestPermissionsAsync();
+    setPermission(granted ? true : false);
+    console.log(granted);
+  };
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log("Type: " + type + "\nData: " + data);
+    setBarcodeNum(`${data}`);
+    console.log(barcodeNum);
+  };
+
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  if (permission === false) {
+    alert("Go to setting and allow camera access");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.textBg}>
+        <Text style={styles.text}>Scan it!</Text>
+      </View>
+      <View style={styles.scannerBox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={styles.scanner}
+        />
+      </View>
+      <View>
+        <Button
+          title={"scan again"}
+          onPress={() => {
+            setScanned(false);
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +56,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textBg: {
+    backgroundColor: "gray",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "black",
+    fontSize: 50,
+  },
+  scannerBox: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    backgroundColor: "tomato",
+    overflow: "hidden",
+  },
+  scanner: {
+    height: 300,
+    width: 300,
   },
 });
