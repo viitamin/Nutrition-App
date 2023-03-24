3f16133fffb7d68b1124dccd30f89e84273f47ad
Min Kim <tmdals0708@gmail.com>
2023-03-25 00:38:27 +0900

    nutriention adding

--- screens/Login.js
+++ screens/Login.js
@@ -1,24 +1,70 @@
 import React, { useState, useEffect } from "react";
 import { Text, TouchableOpacity, View, Button } from "react-native";
 import Barcode from "./Scanner";
-import { BARCODE_NUMBER } from "./Scanner";
 
-const HACCP_API_KEY =
-  "G5ifl7ixJfo7T2HJR6b7P%2FIvr4v6ILirbBv1Yj6rNUEJjZqo0ZLCAV8HWzq2NG8Dx19gpJiJdT%2FFVhddXrDSVw%3D%3D";
-const PRODUCT_NUM_API_KEY = "a89f4295c0c040c4adaa";
-
-//https://openapi.foodsafetykorea.go.kr/api/a89f4295c0c040c4adaa/C005/json/1/100/BAR_CD=`${barcodeNum}`
-//https://apis.data.go.kr/B553748/CertImgListService/getCertImgListService?ServiceKey=G5ifl7ixJfo7T2HJR6b7P%2FIvr4v6ILirbBv1Yj6rNUEJjZqo0ZLCAV8HWzq2NG8Dx19gpJiJdT%2FFVhddXrDSVw%3D%3D&prdlstReportNo=`${ProductNum}`&returnType=json
-
+import { NUTRIENT } from "./Scanner";
 export default function Login({ navigation }) {
-  const [productNum, setProductNum] = useState(null);
-  console.log("바코드: " + BARCODE_NUMBER);
+  const text = NUTRIENT;
+  const calorieRegex = /열량\s?(\d+\.?\d+)/;
+  const carbRegex = /탄수화물\s?(\d+\.?\d+)/;
+  const sugarRegex = /당류\s?(\d+\.?\d+)/;
+  const proteinRegex = /단백질\s?(\d+\.?\d+)/;
+  const fatRegex = /지방\s?(\d+\.?\d+)/;
+  const sFatRegex = /포화지방\s?(\d+\.\d+)/;
+  const tFatRegex = /트랜스지방\s?(\d+\.?\d+)/;
+  const cholesterolRegex = /콜레스테롤\s?(\d+\.?\d+)/;
+  const sodiumRegex = /나트륨\s?(\d+\.?\d+)/;
+
+  const calorieMatch = text.match(calorieRegex);
+  const carbMatch = text.match(carbRegex);
+  const sugarMatch = text.match(sugarRegex);
+  const proteinMatch = text.match(proteinRegex);
+  const fatMatch = text.match(fatRegex);
+  const sFatMatch = text.match(sFatRegex);
+  const tFatMatch = text.match(tFatRegex);
+  const cholesterolMatch = text.match(cholesterolRegex);
+  const sodiumMatch = text.match(sodiumRegex);
+
+  const nutrient = {
+    calorie: calorieMatch ? calorieMatch[1] : 0,
+    carb: carbMatch ? carbMatch[1] : 0,
+    sugar: sugarMatch ? sugarMatch[1] : 0,
+    protein: proteinMatch ? proteinMatch[1] : 0,
+    fat: fatMatch ? fatMatch[1] : 0,
+    sfat: sFatMatch ? sFatMatch[1] : 0,
+    tfat: tFatMatch ? tFatMatch[1] : 0,
+    cholesterol: cholesterolMatch ? cholesterolMatch[1] : 0,
+    sodium: sodiumMatch ? sodiumMatch[1] : 0,
+  };
+
+  console.log(nutrient);
 
   return (
     <View>
       <Text>Login</Text>
-      <Button title={"Hi"} onPress={() => navigation.navigate("Barcode")} />
+      <Button title={"Hi"} onPress={() => navigation.navigate("Scanner")} />
       <Text>Go To Welcome</Text>
+
+      <Text>
+        칼로리: {nutrient.calorie}
+        {"\n"}
+        탄수화물: {nutrient.carb}
+        {"\n"}
+        당류: {nutrient.sugar}
+        {"\n"}
+        단백질: {nutrient.protein}
+        {"\n"}
+        지방: {nutrient.fat}
+        {"\n"}
+        포화지방: {nutrient.sfat}
+        {"\n"}
+        트랜스지방: {nutrient.tfat}
+        {"\n"}
+        콜레스테롤: {nutrient.cholesterol}
+        {"\n"}
+        나트륨: {nutrient.sodium}
+        {"\n"}
+      </Text>
     </View>
   );
 }
--- screens/Scanner.js
+++ screens/Scanner.js
@@ -2,44 +2,54 @@ import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Button, ScrollView } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 
+let NUTRIENT = null;
+const HACCP_API_KEY =
+  "G5ifl7ixJfo7T2HJR6b7P%2FIvr4v6ILirbBv1Yj6rNUEJjZqo0ZLCAV8HWzq2NG8Dx19gpJiJdT%2FFVhddXrDSVw%3D%3D";
+const PRODUCT_NUM_API_KEY = "a89f4295c0c040c4adaa";
+
 export default function Scanner({ navigation }) {
   const [permission, setPermission] = useState(false);
   const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState();
   const [barcodeNum, setBarcodeNum] = useState(null);
-  const [productNum, setProductNum] = useState(null);
   const [nutrientInfo, setNutrientInfo] = useState(null);
+  const [productName, setProductName] = useState(null);
 
   const askForCameraPermission = async () => {
     const { granted } = await BarCodeScanner.requestPermissionsAsync();
     setPermission(granted ? true : false);
     console.log(granted);
   };
-  const handleBarCodeScanned = ({ type, data }) => {
+  const handleBarCodeScanned = async ({ type, data }) => {
     setScanned(true);
-    setText(data);
-    console.log("Type: " + type + " / Data: " + data);
+    console.log("바코드 타입: " + type + " / 바코드 번호: " + data);
     setBarcodeNum(data);
-    console.log(barcodeNum);
+    console.log("tlqk" + barcodeNum);
+    const productNumber = await getProductNumberAPI(data);
+    await getNutritionInfoAPI(productNumber);
   };
   //---------------------------------------------------api 연결 함수---------------------------------------------------------------------------
-  const getProductNumberAPI = async () => {
+  const getProductNumberAPI = async (barcodeNum) => {
     const response = await fetch(
-      `http://openapi.foodsafetykorea.go.kr/api/a89f4295c0c040c4adaa/C005/json/1/100/BAR_CD=88002798` //${barcodeNum}
+      `http://openapi.foodsafetykorea.go.kr/api/${PRODUCT_NUM_API_KEY}/C005/json/1/100/BAR_CD=${barcodeNum}` //${barcodeNum}
     );
     const json = await response.json();
-    console.log(json.C005.row[0].PRDLST_REPORT_NO);
-    setProductNum(json.C005.row[0].PRDLST_REPORT_NO);
-    return json.C005.row[0].PRDLST_REPORT_NO;
+    const productNumber = json.C005.row[0].PRDLST_REPORT_NO;
+    console.log("품목제조보고번호: " + productNumber);
+    return productNumber;
   };
 
-  const getNutritionInfoAPI = async (pn) => {
+  const getNutritionInfoAPI = async (productNum) => {
     const response = await fetch(
-      `https://apis.data.go.kr/B553748/CertImgListService/getCertImgListService?ServiceKey=G5ifl7ixJfo7T2HJR6b7P%2FIvr4v6ILirbBv1Yj6rNUEJjZqo0ZLCAV8HWzq2NG8Dx19gpJiJdT%2FFVhddXrDSVw%3D%3D&prdlstReportNo=1984044800212&returnType=json` //${ProductNum}
+      `https://apis.data.go.kr/B553748/CertImgListService/getCertImgListService?ServiceKey=${HACCP_API_KEY}&prdlstReportNo=${productNum}&returnType=json` //${ProductNum}
     );
     const json = await response.json();
-    console.log(json.body.items[0].item.nutrient);
     setNutrientInfo(json.body.items[0].item.nutrient);
+    setProductName(json.body.items[0].item.prdlstNm);
+    NUTRIENT = json.body.items[0].item.nutrient;
+    const myProduct = json.body.items[0].item.prdlstNm;
+    console.log("상품 이름1: " + productName);
+    console.log("상품 이름2: " + myProduct);
+    console.log(NUTRIENT);
   };
   //---------------------------------------------------api 연결 함수 종료---------------------------------------------------------------------------
 
@@ -48,12 +58,6 @@ export default function Scanner({ navigation }) {
   useEffect(() => {
     askForCameraPermission();
   }, []);
-
-  if (barcodeNum !== null) {
-    useEffect(() => {
-      getNutritionInfoAPI(getProductNumberAPI());
-    }, []);
-  }
 
   return (
     <View style={styles.container}>
@@ -75,9 +79,9 @@ export default function Scanner({ navigation }) {
           }}
         />
       </View>
-      <Text>Barcode Number: {barcodeNum}</Text>
+      <Text>Product name: {productName}</Text>
       <Button
-        title={"I've got numbers go to next page"}
+        title={"I've got numbers go to the next page"}
         onPress={() => navigation.navigate("Login")}
       />
     </View>
@@ -112,3 +116,5 @@ const styles = StyleSheet.create({
     width: 300,
   },
 });
+
+export { NUTRIENT };
